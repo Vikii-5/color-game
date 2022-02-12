dad415dd698f5d79e07b069d50216ec9efa2fe0d
Vikii-5 <vikkycsk3@gmail.com>
2022-02-12 14:03:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useReducer } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddBox />
+    </div>
+  );
+}
+
+const initialState = { color: "blue" };
+
+const reducer = (state, action) => {
+  switch(action.type){
+    case "handleChange":
+      return {color: state.color};
+    default:
+      return state;
+  }
+};
+
+function AddBox() {
+  
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const styles = { background: state.color };
+  console.log(state.color)
+  const handleChange = (event) => event.target.value;
+
+  return (
+    <div>
+      <input
+        value={state.color}
+        style={styles}
+        onChange={() => dispatch({type: handleChange})}
+        placeholder="Enter a Color"
+      />
     </div>
   );
 }
